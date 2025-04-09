c5e9d3f8c8400ebe95ec14ac449ab211bd11f10f
Milan Jana <milanjana14377@gmail.com>
2025-04-09 09:20:09 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,77 @@
+// const mongoose = require("mongoose");
+
+// const ListingSchema = new mongoose.Schema({
+//     // title: String,
+//     title: {
+//         type: String,
+//         required: true,
+//     },
+//     description: String,
+//     image: {
+//         filename: String,
+//         url: String
+//     },
+//     price: Number,
+//     location: String,
+//     country: String
+// });
+
+// module.exports = mongoose.model("Listing", ListingSchema);
+
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const listingSchema = new Schema({
+//     title: {
+//         type: String,
+//         required: true,
+//     },
+//     description: String,
+//     image: {
+//         type: String,
+//         default:
+//         "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+//         set: (v) =>
+//         v === ""
+//             ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+//             : v,
+//     },
+//     price: Number,
+//     location: String,
+//     country: String,
+// });
+
+// const Listing = mongoose.model("Listing", listingSchema);
+// module.exports = Listing;
+
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        filename: {
+            type: String,
+            default: "listingimage"
+        },
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+            set: (v) => 
+                v === "" 
+                    ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" 
+                    : v
+        }
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
